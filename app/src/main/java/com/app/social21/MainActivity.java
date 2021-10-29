471b1cd285a6fb8d69e6eaa9e21478778a6bd402
Priyam Prakash <priyamprakash99@gmail.com>
2021-10-30 00:11:28 +0530

    toolbar Visiblity

--- app/src/main/java/com/app/social21/MainActivity.java
+++ app/src/main/java/com/app/social21/MainActivity.java
@@ -2,11 +2,13 @@ package com.app.social21;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.app.social21.Fragments.AddFragment;
@@ -19,14 +21,22 @@ public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottom_navigation;
     Fragment selectedfragment =  null;
 
+
+     Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        MainActivity.this.setTitle("My Profile");
+
+
         bottom_navigation = findViewById(R.id.bottomBar);
         bottom_navigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
-
+        toolbar.setVisibility(View.GONE);
         FragmentTransaction transaction= getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.fragment_container, new HomeFragment());
         transaction.commit();
@@ -43,19 +53,23 @@ public class MainActivity extends AppCompatActivity {
 
                     switch (item.getItemId()){
                         case R.id.nav_home:
+                            toolbar.setVisibility(View.GONE);
                             selectedfragment = new HomeFragment();
                             break;
                         case R.id.nav_search:
+                            toolbar.setVisibility(View.GONE);
                             selectedfragment = new SearchFragment();
                             break;
                         case R.id.nav_add:
+                            toolbar.setVisibility(View.GONE);
                             selectedfragment = new AddFragment();
                             break;
                         case R.id.nav_notification:
+                            toolbar.setVisibility(View.GONE);
                             selectedfragment = new NotificaionFragment();
                             break;
                         case R.id.nav_profile:
-
+                            toolbar.setVisibility(View.VISIBLE);
                             selectedfragment = new ProfileFragment();
                             break;
 
