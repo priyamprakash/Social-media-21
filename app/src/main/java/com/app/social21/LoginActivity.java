c938efd8bcc2277519f97dd5f60b417aab3210c3
Priyam Prakash <priyamprakash99@gmail.com>
2021-11-01 08:09:40 +0530

    Login Screen

--- app/src/main/java/com/app/social21/LoginActivity.java
+++ app/src/main/java/com/app/social21/LoginActivity.java
@@ -1,24 +1,65 @@
 
 package com.app.social21;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
 
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+    FirebaseUser currentUser;
+    Button login_btn;
+    EditText emailET, passwordET;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        login_btn = findViewById(R.id.login_btn);
 
 
+        emailET = findViewById(R.id.emailET);
+        passwordET = findViewById(R.id.passwordET);
+//       ---------------------------------------------
+        auth = FirebaseAuth.getInstance();
+        database =FirebaseDatabase.getInstance();
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailET.getText().toString();
+                String password = passwordET.getText().toString();
+                auth.signInWithEmailAndPassword(email , password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this, "Logged in as : " + email, Toast.LENGTH_SHORT).show();
+                            Intent i = new Intent(LoginActivity.this , MainActivity.class);
+                            startActivity(i);
+                        }
+                    }
+                });
+
+            }
+        });
     }
 
     public void gotoSignup(View view) {
-        Intent i = new Intent(LoginActivity.this , SignupActivity.class);
+        Intent i = new Intent(LoginActivity.this , LoginActivity.class);
         startActivity(i);
     }
 }
