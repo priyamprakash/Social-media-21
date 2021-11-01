f3e76f3675bde06529254913fb0588107277fb9c
Priyam Prakash <priyamprakash99@gmail.com>
2021-11-01 06:41:35 +0530

    Intent

--- app/src/main/java/com/app/social21/LoginActivity.java
+++ app/src/main/java/com/app/social21/LoginActivity.java
@@ -3,7 +3,9 @@ package com.app.social21;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -13,5 +15,10 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
 
+    }
+
+    public void gotoSignup(View view) {
+        Intent i = new Intent(LoginActivity.this , SignupActivity.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/app/social21/SignupActivity.java
+++ app/src/main/java/com/app/social21/SignupActivity.java
@@ -2,7 +2,9 @@ package com.app.social21;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+    }
+
+    public void gotoLogin(View view) {
+        Intent i = new Intent(SignupActivity.this , LoginActivity.class);
+        startActivity(i);
     }
 }
