105188f892f4af0ec7085889c81240024098e4b5
Priyam Prakash <priyamprakash99@gmail.com>
2021-11-03 02:19:57 +0530

    Add post fragment - fetch profile image , name and profession of the current user

--- app/src/main/java/com/app/social21/Fragments/AddPostFragment.java
+++ app/src/main/java/com/app/social21/Fragments/AddPostFragment.java
@@ -1,7 +1,12 @@
 package com.app.social21.Fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.text.Editable;
@@ -10,12 +15,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.app.social21.Model.User;
 import com.app.social21.R;
 import com.app.social21.databinding.FragmentAddPostBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 public class AddPostFragment extends Fragment {
 
-        FragmentAddPostBinding binding;
+    FragmentAddPostBinding binding;
+    Uri uri;
+    FirebaseAuth auth;
+    FirebaseDatabase database;
 
     public AddPostFragment() {
         // Required empty public constructor
@@ -25,6 +40,9 @@ public class AddPostFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        auth =FirebaseAuth.getInstance();
+        database =FirebaseDatabase.getInstance();
+
 
     }
 
@@ -32,17 +50,46 @@ public class AddPostFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        binding = FragmentAddPostBinding.inflate(inflater , container , false);
+        binding = FragmentAddPostBinding.inflate(inflater, container, false);
+
+//        setting the profile image , username and profession of the current user
+        database.getReference().child("Users").child(FirebaseAuth.getInstance().getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    User user =snapshot.getValue(User.class);
+                    Picasso.get().load(user.getProfilePic()).placeholder(R.drawable.img_default).into(binding.profileImage);
+
+                    binding.name.setText(user.getName());
+                    binding.profession.setText(user.getProfession());
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         binding.postDescription.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                
+
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-
+                String description = binding.postDescription.getText().toString();
+                if (!description.isEmpty()) {
+                    binding.postBtn.setBackgroundDrawable(ContextCompat.getDrawable(getContext(), R.drawable.follow_button_bg));
+                    binding.postBtn.setTextColor(getContext().getResources().getColor(R.color.white));
+                    binding.postBtn.setEnabled(true);
+                } else {
+                    binding.postBtn.setBackgroundDrawable(ContextCompat.getDrawable(getContext(), R.drawable.follow_active_button));
+                    binding.postBtn.setTextColor(getContext().getResources().getColor(R.color.grey));
+                    binding.postBtn.setEnabled(false);
+                }
             }
 
             @Override
@@ -51,6 +98,33 @@ public class AddPostFragment extends Fragment {
             }
         });
 
-        return  binding.getRoot();
+        binding.addImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                get image from the gallery
+                Intent i = new Intent();
+                i.setAction(Intent.ACTION_GET_CONTENT);
+                i.setType("image/*");
+                startActivityForResult(i, 10);
+            }
+        });
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (data.getData() != null) {
+//            set image received from the gallery to the image view - postImage. 
+            uri = data.getData();
+            binding.postImage.setImageURI(uri);
+            binding.postImage.setVisibility(View.VISIBLE);
+            binding.postBtn.setBackgroundDrawable(ContextCompat.getDrawable(getContext(), R.drawable.follow_button_bg));
+            binding.postBtn.setTextColor(getContext().getResources().getColor(R.color.white));
+            binding.postBtn.setEnabled(true);
+
+        }
     }
 }
